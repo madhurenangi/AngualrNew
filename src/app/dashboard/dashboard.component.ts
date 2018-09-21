9b5cc3b78ccaa10d971147ae63c2abee9c3347de
madhurenangi <madhu.dreamjob@gmail.com>
2018-09-21 19:01:59 +0530

    commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,15 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  seat:string="";
+  passenger:any={};
 
   dashArray:any=[
-    {name:"Madhu",age:20,class:"colorDiv"},
-    {name:"Anitha",age:25,class:"colorDiv1"},
-    {name:"Ravi",age:40,class:"colorDiv2"}
+    {name:"",age:"",mobile:"",seat:"1",selected:"",status:""},
+    {name:"",age:"",mobile:"",seat:"2",selected:"",status:""},
+    {name:"",age:"",mobile:"",seat:"3",selected:"",status:""},
+    {name:"",age:"",mobile:"",seat:"4",selected:"",status:""},
+    {name:"",age:"",mobile:"",seat:"5",selected:"",status:""}
+    
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+  showSeat(value){
+    for(var i=0;i<this.dashArray.length;i++){
+      if(this.dashArray[i].seat==value){
+        if(this.dashArray[i].selected=="false" || this.dashArray[i].selected==""){
+        var obj={name:"",age:"",mobile:"",seat:value,selected:"true",status:""};
+        this.dashArray.splice(i,1,obj);
+        }
+        else{
+          var obj={name:"",age:"",mobile:"",seat:value,selected:"false",status:""};
+          this.dashArray.splice(i,1,obj);
+        }
+
+       
+      }
+    }
+    this.passenger.seat=value;
+  }
+  savePassenger(value){
+    for(var i=0;i<this.dashArray.length;i++){
+      if(this.dashArray[i].seat==value.seat){
+        var obj={name:value.name,age:value.age,mobile:value.mobile,seat:value.seat,selected:"true",status:"true"};
+        this.dashArray.splice(i,1,obj);
+        console.log(this.dashArray);
+      }
+    }
+    //this.dashArray.push(value);
   }
 
 
